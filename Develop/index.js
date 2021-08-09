acc50d917a69b31ec8ffb63965344054e66cb2d3
Seth Nightingale <sethnightingale10@gmail.com>
2021-08-09 16:14:38 +0800

    Adding in npm commands and HTML
    -added npm i, npm init and npm i inquirer
    -added html pages per employee type as an outline
    -added template lines for each individual person

--- Develop/index.js
+++ Develop/index.js
@@ -2,3 +2,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+
+//adding in require modules through /lib
+const newIntern = require("./lib/newIntern");
+const newEngineer = require("./lib/newEngineer");
+const newManager = require("./lib/newManager");
