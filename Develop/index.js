192adb6f84503678d2548336bc32d3116a6a8b3f
Seth Nightingale <sethnightingale10@gmail.com>
2021-08-09 17:35:42 +0800

    Trying to get the application working

--- Develop/index.js
+++ Develop/index.js
@@ -14,7 +14,6 @@ const render = require("./lib/createHtml")
 //addding in directory path and logic
 const output = path.resolve(__dirname, "output");
 outputPath = path.join(output, "user.html")
-const render = require("./lib/createHtml");
 
 const questions = [{
         type: "input",
@@ -39,5 +38,81 @@ const questions = [{
         choices: ["Manager", "Engineer", "Intern"],
       },
     ];
+
+    const team = [];
+
+    const generateTeam = () => {
+  inquirer
+    .prompt(questions)
+    .then((ans1) => {
+      inquirer
+        .prompt([
+          {
+            when: () => ans1.base === "Manager",
+            type: "input",
+            message: "What is their office number",
+            name: "officeNumber",
+          },
+          {
+            when: () => ans1.base === "Engineer",
+
+            type: "input",
+            message: "What is the GitHub Username?",
+            name: "github",
+          },
+
+          {
+            when: () => ans1.base === "Intern",
+
+            type: "input",
+            message: "What is the school's name?",
+            name: "school",
+          },
+
+          {
+            type: "confirm",
+            message: "Would you like to add another team member?",
+            name: "addMember",
+          },
+        ])
+
+        .then((ans2) => {
+          if (ans1.base === "Manager") {
+            const manager = new Manager(ans1.name, ans1.id, ans1.email, ans1.base, ans2.officeNumber);
+            team.push(newManager);
+          }
+
+          if (ans1.base === "Engineer") {
+            const engineer = new Engineer(ans1.name, ans1.id, ans1.email, ans1.base, ans2.github);
+            team.push(newEngineer);
+          }
+
+          if (ans1.base === "Intern") {
+            const intern = new Intern(ans1.name, ans1.id, ans1.email, ans1.base, ans2.school);
+            team.push(newIntern);
+          }
+          if (ans2.addMember) {
+            generateTeam();
+          } else {
+            team.forEach((team) => {
+              console.log(team);
+            });
+            fs.writeFile(outputPath, render(team), (err) => {
+              if (err) {
+                throw err;
+              }
+              console.log("Success, team HTML is created!");
+            });
+          }
+        });
+    })
+    .catch((err) => {
+      if (err) {
+        throw err;
+      }
+    });
+};
+
+generateTeam();
     
     
--- Develop/lib/createHtml.js
+++ Develop/lib/createHtml.js
@@ -5,56 +5,56 @@ const createHtml = (people) => {
     const html = []; 
     //filters through the /create file to find "newManager", "newIntern" or "newEngineer"
     html.push(
-        ...people.filter((people) => people.base() === "newManager").map((manager) => renderManager(manager))
+        ...people.filter((people) => people.base() === "newManager").map((newManager) => renderManager(newManager))
     );
     html.push(
-        ...people.filter((people) => people.base() === "newEngineer").map((engineer) => renderEngineer(engineer))
+        ...people.filter((people) => people.base() === "newEngineer").map((newEngineer) => renderEngineer(newEngineer))
     );
     html.push(
-        ...people.filter((people) => people.base() === "newIntern").map((intern) => renderIntern(intern))
+        ...people.filter((people) => people.base() === "newIntern").map((newIntern) => renderIntern(newIntern))
     );
     return renderMain(html.join(""));
 }
 
 //searches for the individual parts of the template html files to retrieve data and append them
-const renderManager = (manager) => {
+const renderManager = (newManager) => {
     let template = fs.readFileSync(path.resolve(templatesDir, "newManager.html"), "utf8");
-    template = replacePlaceholders(template, "name", manager.getName());
-    template = replacePlaceholders(template, "role", manager.getRole());
-    template = replacePlaceholders(template, "email", manager.getEmail());
-    template = replacePlaceholders(template, "id", manager.getId());
-    template = replacePlaceholders(template, "officeNo", manager.getOfficeNo());
+    template = replacePlaceholders(template, "name", newManager.getName());
+    template = replacePlaceholders(template, "base", newManager.getBase());
+    template = replacePlaceholders(template, "email", newManager.getEmail());
+    template = replacePlaceholders(template, "id", newManager.getId());
+    template = replacePlaceholders(template, "officeNo", newManager.getOfficeNo());
     //returns the new edited template
     return template;
   };
 
 //searches for the individual parts of the template html files to retrieve data and append them
-  const renderEngineer = (engineer) => {
-    let template = fs.readFileSync(path.resolve(templatesDir, "newEngineer.html"), "utf8");
-    template = replacePlaceholders(template, "name", engineer.getName());
-    template = replacePlaceholders(template, "role", engineer.getRole());
-    template = replacePlaceholders(template, "email", engineer.getEmail());
-    template = replacePlaceholders(template, "id", engineer.getId());
-    template = replacePlaceholders(template, "github", engineer.getGithub());
+  const renderEngineer = (newEngineer) => {
+    let template = fs.readFileSync(path.resolve(createDir, "newEngineer.html"), "utf8");
+    template = replacePlaceholders(template, "name", newEngineer.getName());
+    template = replacePlaceholders(template, "base", newEngineer.getBase());
+    template = replacePlaceholders(template, "email", newEngineer.getEmail());
+    template = replacePlaceholders(template, "id", newEngineer.getId());
+    template = replacePlaceholders(template, "github", newEngineer.getGithub());
      //returns the new edited template
     return template;
   };
 
   //searches for the individual parts of the template html files to retrieve data and append them
-  const renderIntern = (intern) => {
-    let template = fs.readFileSync(path.resolve(templatesDir, "newIntern.html"), "utf8");
-    template = replacePlaceholders(template, "name", intern.getName());
-    template = replacePlaceholders(template, "role", intern.getRole());
-    template = replacePlaceholders(template, "email", intern.getEmail());
-    template = replacePlaceholders(template, "id", intern.getId());
-    template = replacePlaceholders(template, "school", intern.getSchool());
+  const renderIntern = (newIntern) => {
+    let template = fs.readFileSync(path.resolve(createDir, "newIntern.html"), "utf8");
+    template = replacePlaceholders(template, "name", newIntern.getName());
+    template = replacePlaceholders(template, "base", newIntern.getBase());
+    template = replacePlaceholders(template, "email", newIntern.getEmail());
+    template = replacePlaceholders(template, "id", newIntern.getId());
+    template = replacePlaceholders(template, "school", newIntern.getSchool());
      //returns the new edited template
     return template;
   };
 
   //renders them into the main html page replacing the templates
   const renderMain = (html) => {
-    const template = fs.readFileSync(path.resolve(templatesDir, "index.html"), "utf8");
+    const template = fs.readFileSync(path.resolve(createDir, "index.html"), "utf8");
     return replacePlaceholders(template, "team", html);
   };
 
